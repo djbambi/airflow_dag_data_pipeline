264ff8323de6ea2112caa735d6f787bb4551af4a
David Bambrough <djbambrough@mac.com>
2026-01-29 20:04:44 +0000

    Added logger to weather_client.py

--- src/airflow_dag_data_pipeline/weather_client.py
+++ src/airflow_dag_data_pipeline/weather_client.py
@@ -1,13 +1,18 @@
 """Client to fetch weather data from OpenWeather API"""
 
+import platform
 from typing import Any
 
 import requests
+
+from airflow_dag_data_pipeline import logger
 
 DEFAULT_HEADERS: dict[str, str] = {
     "User-Agent": "WeatherApp/1.0",
     "Accept": "application/json",
 }
+
+log = logger.get_logger(f"{platform.node()} - Some System")
 
 
 def fetch_openweather_data(
@@ -17,6 +22,7 @@ def fetch_openweather_data(
     params: dict[str, str | int | float],
     timeout_s: float,
 ) -> dict[str, Any]:
+    log.info("Fetching data from OpenWeather API.")
     response = session.get(url, params=params, timeout=timeout_s)
     response.raise_for_status()
     return response.json()  # type: ignore[no-any-return]
