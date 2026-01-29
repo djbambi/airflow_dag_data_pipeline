984a4e77366b1ec9f79b931b6d3780b3ef33e593
David Bambrough <djbambrough@mac.com>
2026-01-29 19:49:24 +0000

    Logger added

---
+++ src/airflow_dag_data_pipeline/logger/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+
+def get_logger(name):
+    log = logging.getLogger(name)
+    handler = logging.FileHandler(filename="logs/log.txt", mode="a")
+
+    format_str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    formatter = logging.Formatter(fmt=format_str, datefmt="%Y-%m-%d %H:%M:%S%z")
+    handler.formatter = formatter
+    log.addHandler(handler)
+    log.setLevel(logging.INFO)
+
+    return log
