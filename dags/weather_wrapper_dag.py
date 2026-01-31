05be4994ff1e54cbe0e0d00223169b448d6b3c20
David Bambrough <djbambrough@mac.com>
2026-01-31 07:44:03 +0000

    Add missing newline for code style consistency in DAG and test files

--- dags/weather_wrapper_dag.py
+++ dags/weather_wrapper_dag.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+
 from airflow_dag_data_pipeline.main import main
 
 with DAG(
@@ -9,7 +10,7 @@ with DAG(
     start_date=datetime(2025, 1, 1),
     schedule=None,  # manual trigger for now (simplest)
     catchup=False,
-) as dag:
+) as dag:  # noqa: F841
     run = PythonOperator(
         task_id="run_main",
         python_callable=main,
