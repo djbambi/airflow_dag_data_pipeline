0633c22eaa79389e3c4c9d087d485a98113591a6
David Bambrough <djbambrough@mac.com>
2026-02-22 20:00:59 +0000

    Add PandasWeatherDataTransformer to public API in __all__ declaration

--- src/airflow_dag_data_pipeline/__init__.py
+++ src/airflow_dag_data_pipeline/__init__.py
@@ -1,3 +1,4 @@
+from airflow_dag_data_pipeline.pandas_transformer import PandasWeatherDataTransformer
 from airflow_dag_data_pipeline.weather_transformer import WeatherDataTransformer
 
-__all__ = ["WeatherDataTransformer"]
+__all__ = ["WeatherDataTransformer", "PandasWeatherDataTransformer"]
